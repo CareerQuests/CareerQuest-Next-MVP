ce8c51a9e4da01469021c7ec7109bff467f3be0c
Adhil-Akbar <356adhil@gmail.com>
2024-10-25 23:35:04 +0530

    added pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import nextPwa from "next-pwa";
 
-export default nextConfig;
+const withPWA = nextPwa({
+  dest: "public", // Specify where to output the service worker file
+  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+});
+
+export default withPWA({
+  // Other Next.js configuration options can go here
+});
