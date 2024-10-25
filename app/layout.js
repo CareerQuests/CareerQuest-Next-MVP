bb17518501428533514d8131f39dce655db7b882
Adhil-Akbar <356adhil@gmail.com>
2024-10-25 23:52:49 +0530

    Added logo

--- app/layout.js
+++ app/layout.js
@@ -22,8 +22,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/compass-regular.png" type="image/png" />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon-192x192.png" />
+        <link rel="apple-touch-icon" href="/compass-regular.png" />
         <meta name="theme-color" content="#000000" />
       </head>
       <body
