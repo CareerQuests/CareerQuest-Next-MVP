48ff8f614b7505305fc8bcd1fb9a7c2705c1dcf2
Adhil-Akbar <356adhil@gmail.com>
2024-10-25 23:40:14 +0530

    updated project

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+// /app/layout.js
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -20,6 +21,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon-192x192.png" />
+        <meta name="theme-color" content="#000000" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
